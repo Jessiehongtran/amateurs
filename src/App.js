083c3b946cf53737700671b1def586229fe0604b
Your Name <you@example.com>
2020-08-30 11:05:56 -0700

    done signup form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import Nav from './components/nav';
 import './App.css';
 import Main from './components/main';
 import SignUp from './components/signup';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav  />
-      {/* <SignUp/> */}
-      <Main />
-      
+      <Route
+        exact path="/"
+        render={
+          props => {
+            return (
+              <>
+                <Nav  {...props}/>
+                <Main {...props}/>
+              </>
+            )
+          }
+        } 
+      />
+      <Route
+        exact path="/signup"
+        render={
+          props => {
+            return (
+              <>
+                <SignUp  {...props}/>
+              </>
+            )
+          }
+        } 
+      />
     </div>
   );
 }
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/signup.scss';
 
 export default class SignUp extends React.Component {
     constructor(props){
@@ -26,20 +27,28 @@ export default class SignUp extends React.Component {
 
     render(){
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="email"
-                        name="email"
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={this.handleChange}
-                    />
-                    <button>Sign Up</button>
-                </form>
+            <div className="signup">
+                <div className="signup-box">
+                    <form onSubmit={this.handleSubmit}>
+                        <h3>Sign Up Now</h3>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={this.handleChange}
+                        />
+                        <button className="signup-btn">Sign Up</button>
+                    </form>
+                    <p className="split"><span> Or </span></p>
+                    <button className="google-btn">Log in with Google</button>
+                    <p className="signin-ask">Do you have an account? <a href="#">Sign In</a></p>
+                </div>
             </div>
         )
     }
