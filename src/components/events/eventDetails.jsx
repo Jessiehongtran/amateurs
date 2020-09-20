4cebf8f55cf6185883b6c22f250c87e67a1ce62c
Your Name <you@example.com>
2020-09-20 14:23:30 -0700

    styled more for event page

--- src/components/events/eventDetails.jsx
+++ src/components/events/eventDetails.jsx
@@ -3,7 +3,23 @@ import './eventDetails.scss'
 
 export default class EventDetails extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            comments: ["very nice guys", "ok ok"],
+            comment: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e){
+        this.setState({comment: e.target.value})
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        this.setState({comments: [...this.state.comments, this.state.comment]})
     }
 
     render(){
@@ -17,13 +33,18 @@ export default class EventDetails extends React.Component {
                 <div className="access">
                     <p className="banner">Join event</p>
                     <div className="comments">
-                        <input
-                            placeholder="Leave a comment" 
-                        />
-                        <div className="comment">
-                            <img className="avatar" src="https://res.cloudinary.com/zofuku/image/upload/v1600635037/Profile-PNG-Icon_wdgkjg.png"/>
-                            <p>hahaha</p>
-                        </div>
+                        <form onSubmit={this.handleSubmit}>
+                            <input
+                                placeholder="Leave a comment"
+                                onChange={this.handleChange} 
+                            />
+                        </form>
+                        {this.state.comments.map(text => 
+                            <div className="comment">
+                                <img className="avatar" src="https://res.cloudinary.com/zofuku/image/upload/v1600635037/Profile-PNG-Icon_wdgkjg.png"/>
+                                <p className="text">{text}</p>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
